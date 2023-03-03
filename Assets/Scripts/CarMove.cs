27d725cc02e8dee14b89828df13c4e941f85aad7
Pranjali Yadav <yadavpranjali1223@gmail.com>
2023-03-03 19:48:49 +0530

    second

--- Assets/Scripts/CarMove.cs
+++ Assets/Scripts/CarMove.cs
@@ -7,10 +7,20 @@ public class CarMove : MonoBehaviour
 {
     [SerializeField] float speed = 10f;
     [SerializeField] float speedGain = 0.2f;
+    [SerializeField] private float turnSpeed = 200f;
+    private int steerValue;
 
     private void Update()
     {
         speed += speedGain*Time.deltaTime;
+
+        transform.Rotate(0f, steerValue*turnSpeed * Time.deltaTime, 0f);
+
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    public void Steer(int value)
+    {
+        steerValue = value;
     }
 }
