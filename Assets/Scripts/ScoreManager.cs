af017e809708cd7a85ef06e5f73dd6101c9aa62a
Pranjali Yadav <yadavpranjali1223@gmail.com>
2023-03-04 23:10:33 +0530

    highScore

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -8,10 +8,20 @@ public class ScoreManager : MonoBehaviour
     [SerializeField] private TMP_Text scoreText;
     [SerializeField] private float scoreMultiplier;
     private float score;
+    public const string HIGH_SCORE_KEY = "HighScore";
     private void Update()
     {
         score += Time.deltaTime*scoreMultiplier;
         
         scoreText.text = Mathf.FloorToInt(score).ToString();
+    }
+    private void OnDestroy()
+    {
+        int currentHighScore = PlayerPrefs.GetInt(HIGH_SCORE_KEY, 0); //default value is if we cant get value of anything then what we return, in that case return 0
+
+        if (score > currentHighScore)
+        {
+            PlayerPrefs.SetInt(HIGH_SCORE_KEY, Mathf.FloorToInt(score));
+        }
     }
 }
