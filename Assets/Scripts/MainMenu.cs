4c530c3c6b9c8aae88e4c98931228242036ab6f5
Pranjali Yadav <yadavpranjali1223@gmail.com>
2023-03-05 11:10:12 +0530

    energy system correctedd

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
@@ -11,13 +12,24 @@ public class MainMenu : MonoBehaviour
     [SerializeField] private TMP_Text energyText;
     [SerializeField] private int maxEnergy;
     [SerializeField] private int energyRechargeDuration;
+    [SerializeField] private Button playBtn;
     [SerializeField] private AndroidNotificationHandler androidNotificationHandler;
     [SerializeField] private iOSNotificationHandler iOSNotificationHandler;
     private int energy;
     private const string ENERGY_KEY = "Energy";
     private const string ENERGY_READY_KEY = "EnergyReady";
+    private const string INVOKE_FUNC = "EnergyRecharged";
+
     private void Start()
     {
+        OnApplicationFocus(true);
+    }
+    private void OnApplicationFocus(bool hasFocus)
+    {
+        if (!hasFocus) { return; }
+
+        CancelInvoke();
+
         int highScore = PlayerPrefs.GetInt(ScoreManager.HIGH_SCORE_KEY, 0);
 
         highScoreText.text = $"High Score {highScore}";
@@ -37,8 +49,20 @@ public class MainMenu : MonoBehaviour
                 energy = maxEnergy;
                 PlayerPrefs.SetInt(ENERGY_KEY, energy);
             }
+            else //figure how how long it takes to get ready while we on main menu
+            {
+                playBtn.interactable = false;
+                Invoke(nameof(EnergyRecharged) ,(energyReady - DateTime.Now).Seconds); //run fuunc in some seconds
+            }
         }
+        energyText.text = $"Play ({energy})";
+    }
 
+    private void EnergyRecharged()
+    {
+        playBtn.interactable = true;
+        energy = maxEnergy;
+        PlayerPrefs.SetInt(ENERGY_KEY, energy);
         energyText.text = $"Play ({energy})";
     }
     public void Play()
